5491b954ab7cf9f9a7550642f0c5cb2e598bb1ae
Sambego <sambellen@gmail.com>
2018-12-10 22:45:21 +0100

    Updates

---
+++ precache-manifest.025bb2377719a45dcc1f25718f69aadf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "139ebcfa27777b9c754d16476ee65f83",
+    "url": "/diorama-demo/static/media/sam.139ebcfa.jpg"
+  },
+  {
+    "revision": "1f2f99c6ea262ce064fe26d36242679f",
+    "url": "/diorama-demo/static/media/poes.1f2f99c6.jpg"
+  },
+  {
+    "revision": "85b5e9f92382ed144f4e48a82b376428",
+    "url": "/diorama-demo/static/media/orca.85b5e9f9.jpg"
+  },
+  {
+    "revision": "5bec1ccd830b96a6ef02b70fe6fce84f",
+    "url": "/diorama-demo/static/media/husky.5bec1ccd.jpg"
+  },
+  {
+    "revision": "ef8f4a5272a911a5bb54d6514ed5d015",
+    "url": "/diorama-demo/static/media/cap.ef8f4a52.jpg"
+  },
+  {
+    "revision": "2149184308579155309a",
+    "url": "/diorama-demo/static/js/runtime~main.21491843.js"
+  },
+  {
+    "revision": "693df5244849ba5688e8",
+    "url": "/diorama-demo/static/js/main.693df524.chunk.js"
+  },
+  {
+    "revision": "09d048f504128dfb5bdc",
+    "url": "/diorama-demo/static/js/1.09d048f5.chunk.js"
+  },
+  {
+    "revision": "693df5244849ba5688e8",
+    "url": "/diorama-demo/static/css/main.856cc382.chunk.css"
+  },
+  {
+    "revision": "6682282132c79acf734136a2ccbc6918",
+    "url": "/diorama-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/diorama-demo/precache-manifest.8562131a45b602dfde4ce7838f4aaafa.js"
+  "/diorama-demo/precache-manifest.025bb2377719a45dcc1f25718f69aadf.js"
 );
 
 workbox.clientsClaim();
